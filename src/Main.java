9f9e22cb1eeee1bc23f88d139313a379f7b21be3
Ahmad Meer <Amm752@nau.edu>
2016-05-15 19:10:29 -0700

    Restart

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ahmad on 5/15/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+
+    }
+}
