f47c71e4f7849aaa61fd51214ed0fb19c7a69296
Ahmad Meer <Amm752@nau.edu>
2016-05-15 19:32:39 -0700

    Adding Comments

--- src/Interface.java
+++ src/Interface.java
@@ -2,4 +2,9 @@
  * Created by ahmad on 5/15/2016.
  */
 public class Interface {
+
+    // init board with places of postion
+
+
+
 }
--- src/Models.java
+++ src/Models.java
@@ -2,4 +2,14 @@
  * Created by ahmad on 5/15/2016.
  */
 public class Models {
+    // Grap the id, hp, type, from the database
+
+    // Function for postion
+
+    // Function for Defeting a poke
+        // else Freez
+
+    // Function for Support Poke
+
+
 }
