8bed1d913dde545145cb39332eed223687b6e1c6
Ahmad Meer <Amm752@nau.edu>
2016-05-16 21:05:13 -0700

    Updated the boardGrid

--- src/Interface.java
+++ src/Interface.java
@@ -1,10 +1,113 @@
 /**
  * Created by ahmad on 5/15/2016.
  */
+
 public class Interface {
 
-    // init board with places of postion
 
+
+
+
+
+    public static void displayBoard() {
+        String line = "";
+        System.out.println();
+        line = "A|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "B|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "C|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "D|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "E|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "F|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "G|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "H|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "I|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+        line = "J|" + "  " + " | " + " | " + " | " + " | "+ " | " + " | " + " | "+ " | " + " | " + " | ";
+        System.out.println(line);
+        System.out.println("----------------------------------");
+    }
+
+
+
+
+
+
+    /**
+    init board with places of postion
+    static int A1, A2, A3, B1, B2, B3, C1, C2, C3, D1, D2, D3, E1, E2, E3, F1, F2, F3, H1, H2, H3, I1, I2, I3, J1, J2, J3;
+    String Playerone = "";
+    String Playertwo = "";
+    boolean gameIsWon = false;
+
+
+
+
+
+    public static void displayBoard()
+    {
+        String line = "";
+        System.out.println();
+        line = " "  + " | " +   " | " +  " | " +  " | " ;
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        line = " "  + " | " + " | " + " | "  + " | " ;
+        System.out.println(line);
+        System.out.println("-------------------------------");
+
+
+        /*line = " " + getXO(C1) + " | " + getXO(C2) + " | " + getXO(C3) + " | " + getXO(C4) + " | " + getXO(C5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println();
+        line = " " + getXO(D1) + " | " + getXO(D2) + " | " + getXO(D3) + " | " + getXO(D4) + " | " + getXO(D5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println();
+        line = " " + getXO(E1) + " | " + getXO(E2) + " | " + getXO(E3) + " | " + getXO(E4) + " | " + getXO(E5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println();
+        line = " " + getXO(F1) + " | " + getXO(F2) + " | " + getXO(F3) + " | " + getXO(F4) + " | " + getXO(F5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println();
+        line = " " + getXO(G1) + " | " + getXO(G2) + " | " + getXO(G3) + " | " + getXO(G4) + " | " + getXO(G5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println();
+        line = " " + getXO(H1) + " | " + getXO(H2) + " | " + getXO(H3) + " | " + getXO(H4) + " | " + getXO(H5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println();
+        line = " " + getXO(I1) + " | " + getXO(I2) + " | " + getXO(I3) + " | " + getXO(I4) + " | " + getXO(I5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println();
+        line = " " + getXO(J1) + " | " + getXO(J2) + " | " + getXO(J3) + " | " + getXO(J4) + " | " + getXO(J5);
+        System.out.println(line);
+        System.out.println("-------------------------------");
+        System.out.println(); */
 
 
 }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
-/**
- * Created by ahmad on 5/15/2016.
- */
+
+
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        Scanner inputName = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String userInput = inputName.nextLine();
 
-
+        System.out.println(userInput);
+        Interface.displayBoard();
     }
+
+
 }
