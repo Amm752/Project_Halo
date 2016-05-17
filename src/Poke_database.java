1734cc40976c4d92e020baaa217eb91aee0fd5bc
Lee-sayaka <lee000sayaka@gmail.com>
2016-05-17 00:01:58 -0600

    data upload

--- src/Poke_database.java
+++ src/Poke_database.java
@@ -38,5 +38,37 @@ public class Poke_database
 
         }
     }
+    public String getHp(String x)
+    {
+        String fileName = "poke_data_base2.txt";
+        String line = null;
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                System.out.println(line);
+                String [] words = line.split(" ");
+                if (words[0] == x)
+                {
+                    return words[5];
+                }
+                // System.out.println(words[1]);
+            }
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex)
+        {
+            System.out.println("Unable to open file '" + fileName + "'");
+        }
+
+        catch(IOException ex)
+        {
+            System.out.println("Error reading file '" + fileName + "'");
+
+        }
+        return ;
+    }
 
 }
